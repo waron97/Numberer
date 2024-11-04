1b2652c38a613eae1b56bb4536945381040bb74c
waron97 <winkler.aron5gmail.com>
2024-11-04 20:46:15 +0100

    Initial Commit

---
+++ Numberer/NumbererApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbererApp.swift
+//  Numberer
+//
+//  Created by Aron Winkler on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NumbererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
